0dd3772f3f504813f1f1cf60783d5de0b36b5a7a
Julie V <julievitse91@gmail.com>
2024-12-27 13:04:09 +0100

    updated models

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const connectDB = require('../db'); // Assuming this file is in a separate folder
 
 const app = express();
-const port = process.env.PORT || 5000; // Use environment variable for port
+const port = process.env.PORT || 5001; // Use environment variable for port
 
 const Quizzes = require('../model'); // Assuming you have a Quiz model defined in models/quiz.js
 const UserQuizzes = require('../userModel'); // Assuming you have a UserQuiz model defined in models/userQuiz.js
@@ -26,12 +26,12 @@ app.get('/api/quizzes', async (req, res) => {
 
 app.get('/api/quizzes/:id', async (req, res) => {
     try {
-        const quiz = await Quizzes.find( 
-            { id: parseInt(req.params.id) }
-        ); // Fetch a quiz by ID
+        const quiz = await Quizzes.findById(req.params.id); // Fetch a quiz by ID
         if (!quiz) {
             return res.status(404).json({ msg: 'Quiz not found' });
         }
+        console.log(quiz);
+        console.log(quiz._id);
         res.json(quiz);
     } catch (err) {
         console.error(err.message);
